1d40880aee96478abf863fe8688090320d0ad899
leonardovin <mcleozin4u@gmail.com>
2021-07-19 18:31:16 -0300

    added mongoDB and mongoose

--- online-store/server/src/app.js
+++ online-store/server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require ('cors')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(morgan('combine'))
@@ -8,6 +9,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
+//conecta ao banco de dados
+mongoose.connect('mongodb+srv://admin:123@cluster0.vuut3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+{useNewUrlParser: true , 
+useUnifiedTopology: true
+})
+
+//carrega rotas
 require('./routes')(app)
 
 app.listen(process.env.PORT || 8081)
