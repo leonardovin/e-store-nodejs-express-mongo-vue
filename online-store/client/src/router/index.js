1fa3ed0d1d13c87c4bbe75ddf5d27578e7d837cd
Igor_Resende <igor.resende@radargovernamental.com.br>
2021-07-26 18:04:33 -0300

    search

--- online-store/client/src/router/index.js
+++ online-store/client/src/router/index.js
@@ -1,60 +1,65 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Landing from '@/components/Landing'
-import Register from '@/components/Register'
-import Login from '@/components/Login'
-import Cart from '@/components/Carrinho'
-import Products from '@/components/Products'
-import CreateProduct from '@/components/CreateProduct'
-import ProductPage from '@/components/ProductPage'
-import ProductsCat from '@/components/ProductsCat'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Router from "vue-router";
+import Landing from "@/components/Landing";
+import Register from "@/components/Register";
+import Login from "@/components/Login";
+import Cart from "@/components/Carrinho";
+import Products from "@/components/Products";
+import CreateProduct from "@/components/CreateProduct";
+import ProductPage from "@/components/ProductPage";
+import ProductsCat from "@/components/ProductsCat";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'landing',
-            component: Landing
-        },
-        {
-            path: '/register',
-            name: 'register',
-            component: Register
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: Login
-        },
-        {
-            path: '/cart',
-            name: 'cart',
-            component: Cart
-        },
-        {
-            path: '/products/category/:productCategory',
-            name: 'products-cat',
-            component: ProductsCat
-        },
-        {
-            path: '/products',
-            name: 'products',
-            component: Products
-        },
-        {
-            path: '/products/create',
-            name: 'create-product',
-            component: CreateProduct
-        },
-        {
-            path: '/products/:productId',
-            name: 'product-page',
-            component: ProductPage
-        }
-    ]
-})
+	routes: [
+		{
+			path: "/",
+			name: "landing",
+			component: Landing
+		},
+		{
+			path: "/register",
+			name: "register",
+			component: Register
+		},
+		{
+			path: "/login",
+			name: "login",
+			component: Login
+		},
+		{
+			path: "/cart",
+			name: "cart",
+			component: Cart
+		},
+		{
+			path: "/products/category/:productCategory",
+			name: "products-cat",
+			component: ProductsCat
+		},
+		{
+			path: "/products",
+			name: "products",
+			component: Products
+		},
+		{
+			path: "/products/search",
+			name: "search",
+			component: Products
+		},
+		{
+			path: "/products/create",
+			name: "create-product",
+			component: CreateProduct
+		},
+		{
+			path: "/products/:productId",
+			name: "product-page",
+			component: ProductPage
+		}
+	]
+});
--- online-store/server/src/routes.js
+++ online-store/server/src/routes.js
@@ -1,32 +1,29 @@
-const authController = require("./controllers/auth-controller")
+const authController = require("./controllers/auth-controller");
 //const authControllerPolicy = require('./policies/authControllerPolicy')
-const prodController = require("./controllers/products-controller")
+const prodController = require("./controllers/products-controller");
 
 module.exports = (app) => {
-    //register
-    app.post('/register',
-        //authControllerPolicy.register,  TODO
-        authController.register)
-    //login
-    app.post('/login',
-        authController.login)
+  //register
+  app.post(
+    "/register",
+    //authControllerPolicy.register,  TODO
+    authController.register
+  );
+  //login
+  app.post("/login", authController.login);
 
-    //post products
-    app.post('/products',
-        prodController.post)
-    //get products
-    app.get('/products',
-        prodController.index)
-    //get products landing page
-    app.get('/',
-        prodController.indexMP)
-    //get a product
-    app.get('/products/:id',
-        prodController.show)
-    //get products by category
-    app.get('/products/category/:category',
-        prodController.indexByCat)
-    //add product to a cart
-    app.put('/cart/:id',
-        prodController.putInCart)
-}
+  //post products
+  app.post("/products", prodController.post);
+  //get products
+  app.get("/products", prodController.index);
+  //search products
+  app.get("/products/search", prodController.index);
+  //get products landing page
+  app.get("/", prodController.indexMP);
+  //get a product
+  app.get("/products/:id", prodController.show);
+  //get products by category
+  app.get("/products/category/:category", prodController.indexByCat);
+  //add product to a cart
+  app.put("/cart/:id", prodController.putInCart);
+};
